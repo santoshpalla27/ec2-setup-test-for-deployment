b317a8264273d8f7b8501f07d42afd06dee4c779
santoshpalla27 <santoshpalla2002@gmail.com>
2025-06-30 15:39:35 +0530

    app

--- mern/frontend/server.js
+++ mern/frontend/server.js
@@ -16,9 +16,27 @@ app.get('/api/frontend-hostname', (req, res) => {
   });
 });
 
-// Serve the main page
+// Serve the main page with backend URL injection
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  const backendUrl = process.env.BACKEND_URL || 'http://localhost:3001';
+  const indexPath = path.join(__dirname, 'public', 'index.html');
+  
+  // Read the HTML file and inject the backend URL
+  const fs = require('fs');
+  fs.readFile(indexPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error loading page');
+    }
+    
+    // Inject the backend URL as a global variable
+    const modifiedHtml = data.replace(
+      '<script>',
+      `<script>
+        window.BACKEND_URL = '${backendUrl}';`
+    );
+    
+    res.send(modifiedHtml);
+  });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
